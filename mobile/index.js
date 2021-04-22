31f6b78543c466fa837c19f5a40477807bb7292c
HiroIWL <hiroiwl@gmail.com>
2021-04-22 13:45:42 -0300

    update
    JavaScript on 'Status' and 'Notificações'

--- mobile/index.js
+++ mobile/index.js
@@ -33,63 +33,105 @@ function botoes() {
   botoes();
 
 
+
+ 
+
+
+
   window.onload = () => {
     nome_aleatorio_m();
     nome_aleatorio_n();
     nome_aleatorio_t();
-    resultado_aleatorio();
+    
 }
 
 
 
 // funções que geram strings aleatórias para o gráfico de porcentagem
 
+
+  function porcentagem_Aleatoria() {
+    let numero = Math.round(Math.random() * 100 )
+    return numero;
+    
+   
+}
+
+
+
   function nome_aleatorio_t() {
-    let lista_de_nomes = [ "0%","5%", "10%", "15%", "20%", "25%", "30%", "35%", "40%", "45%", "50%", "55%", "60%", "65%", "70%", "75%", "80%", "85%", "90%", "95%", "100%" ]
-    let tamanho = lista_de_nomes.length;
-    let posicao = Math.floor(Math.random() * tamanho)
-    let nome = lista_de_nomes [posicao];
+   
 
-    console.log(lista_de_nomes)
-
-    document.querySelector("#legenda-t").textContent = nome;
-
+    document.querySelector("#legenda-t").textContent = porcentagem_Aleatoria() + "%";
+    
+    
 }
 
 
 function nome_aleatorio_n() {
-  let lista_de_nomes = ["0%","5%", "10%", "15%", "20%", "25%", "30%", "35%", "40%", "45%", "50%", "55%", "60%", "65%", "70%", "75%", "80%", "85%", "90%", "95%", "100%"]
-  let tamanho = lista_de_nomes.length;
-  let posicao = Math.floor(Math.random() * tamanho)
-  let nome = lista_de_nomes [posicao];
 
-  console.log(lista_de_nomes)
 
-  document.querySelector("#legenda-n").textContent = nome;
-
+  document.querySelector("#legenda-n").textContent = porcentagem_Aleatoria() + "%";
+ 
 }
 
 function nome_aleatorio_m() {
-  let lista_de_nomes = ["0%","5%", "10%", "15%", "20%", "25%", "30%", "35%", "40%", "45%", "50%", "55%", "60%", "65%", "70%", "75%", "80%", "85%", "90%", "95%", "100%"]
-  let tamanho = lista_de_nomes.length;
-  let posicao = Math.floor(Math.random() * tamanho)
-  let nome = lista_de_nomes [posicao];
 
-  console.log(lista_de_nomes)
 
-  document.querySelector("#legenda-m").textContent = nome;
+  document.querySelector("#legenda-m").textContent = porcentagem_Aleatoria() + "%";
+ 
+  
 
 }
 
 // função que irá gerar resultados diários do estado geral da coluna
 
-function resultado_aleatorio() {
-  let lista_de_nomes = ["Razoável","Ruim","Excelente"]
-  let tamanho = lista_de_nomes.length;
-  let posicao = Math.floor(Math.random() * tamanho)
-  let nome = lista_de_nomes [posicao];
 
-  console.log(lista_de_nomes)
 
-  document.querySelector("#resultado").textContent = nome;
+function calcular_media () {
+ let manhã = document.querySelector("#legenda-m").value;
+ let tarde = document.querySelector("#legenda-t").value;
+ let noite = document.querySelector("#legenda-n").value;
+
+   manhã = parseInt(manhã);
+   tarde = parseInt(tarde);
+   noite = parseInt(noite);
+
+
+  let media = (manhã + tarde + noite)/ 3;
+  
+  let resultado = null;
+
+  if (media >= 75) {
+    resultado = "Ruim"
+  }
+  else if (media < 75 && media > 25 ) {
+    resultado = "Razoável"
+  }
+  else if (media <= 25 ) {
+    resultado = "Excelente"
+  }
+
+  console.log(manhã, tarde, noite);
+  console.log(media);
+  console.log(resultado);
+
+  let span_resultado = document.querySelector("#resultado");
+  span_resultado.textContent = resultado;
 }
+
+// botão para calcular a média
+let btn_calcular_media = document.querySelector(".calcular-media");
+btn_calcular_media.onclick = () => { calcular_media(); }
+
+
+
+
+let notif = document.querySelector(".not");
+let btn = document.getElementById('limpar');
+btn.addEventListener('click', () => {
+ notif.classList.replace('not', 'remover');
+})
+
+
+
